f38d60c5cb27a835ac058b696a138f49bc6ba878
clarisa <cortega2@colostate.edu>
2026-04-18 14:53:31 -0600

    ball controller file

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks.Dataflow;
+using UnityEngine;
+
+public class BallController : MonoBehaviiour
+{
+    public float speed = 5f;
+    private Vector3 direction;
+    private Vector3 dtartPosition;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+    void Update()
+    {
+        if (!isMoving) return;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    public void Launch()
+    {
+        direction = Vector3.right;
+        isMoving = true;
+    }
+
+    public void Stop()
+    {
+        isMoving = false;
+    }
+
+    public void ResetBall()
+    {
+        Stop();
+        transform.position = startPosition;
+    }
+    public void Bounce()
+    {
+        direction = -direction;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            Bounce();
+        }
+    }
+}
