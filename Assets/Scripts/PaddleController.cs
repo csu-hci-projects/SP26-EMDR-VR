253f28f8a6f764bf2850533dd23ca79a7f7b8bd8
clarisa <cortega2@colostate.edu>
2026-05-02 20:57:43 -0600

    can't see controllers on meta

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -13,23 +13,37 @@ public class PaddleController : MonoBehaviour
     public float yMax = 2.5f;
 
     private XRInputDevice controller;
+    private bool controllerFound = false;  
     private bool wasButtonPressed = false;
 
     void Start()
     {
-        var characteristics = isLeftController
-            ? InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller
-            : InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-
-        var devices = new List<XRInputDevice>();
-        InputDevices.GetDevicesWithCharacteristics(characteristics, devices);
-        if (devices.Count > 0) controller = devices[0];
     }
 
     void Update()
     {
+        if (!controllerFound)
+            TryFindController();
+
         HandleStartStop();
         HandlePaddleMovement();
+    }
+
+    void TryFindController()
+    {
+        var characteristics = isLeftController
+            ? InputDeviceCharacteristics.Left  | InputDeviceCharacteristics.Controller
+            : InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+
+        var devices = new List<XRInputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(characteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            controller = devices[0];
+            controllerFound = true;
+            Debug.Log($"[PaddleController] {(isLeftController ? "Left" : "Right")} controller found: {controller.name}");
+        }
     }
 
     void HandleStartStop()
@@ -39,7 +53,9 @@ public class PaddleController : MonoBehaviour
         #if UNITY_EDITOR
             buttonPressed = Keyboard.current.spaceKey.wasPressedThisFrame;
         #else
-            controller.TryGetFeatureValue(XRCommonUsages.primaryButton, out buttonPressed);
+            // Only read input if we actually have a controller
+            if (controllerFound)
+                controller.TryGetFeatureValue(XRCommonUsages.primaryButton, out buttonPressed);
         #endif
 
         if (buttonPressed && !wasButtonPressed)
@@ -64,18 +80,21 @@ public class PaddleController : MonoBehaviour
         #if UNITY_EDITOR
             if (isLeftController)
             {
-                if (Keyboard.current.upArrowKey.isPressed) moveY = 1f;
+                if (Keyboard.current.upArrowKey.isPressed)   moveY =  1f;
                 if (Keyboard.current.downArrowKey.isPressed) moveY = -1f;
             }
             else
             {
-                if (Keyboard.current.wKey.isPressed) moveY = 1f;
+                if (Keyboard.current.wKey.isPressed) moveY =  1f;
                 if (Keyboard.current.sKey.isPressed) moveY = -1f;
             }
         #else
-            Vector2 joystick;
-            controller.TryGetFeatureValue(XRCommonUsages.primary2DAxis, out joystick);
-            moveY = joystick.y;
+            if (controllerFound)
+            {
+                Vector2 joystick;
+                controller.TryGetFeatureValue(XRCommonUsages.primary2DAxis, out joystick);
+                moveY = joystick.y;
+            }
         #endif
 
         Vector3 pos = transform.position;
